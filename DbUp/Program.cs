68e9fb485f5319367d3eaf4f09a9cb91b78f9e54
Alexandrea Beh <abeh@navexglobal.com>
2016-04-02 21:44:51 -0700

    Initial commit - MVC initial web app - initial tables created - UI experiments in progress - contains DbUp, FakeItEasy, FontAwesome - using Azure SQL Server

---
+++ DbUp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbUp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var connectionString = 
+                ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+                #if DEBUG
+                Console.ReadLine();
+                #endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
